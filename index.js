2cc58590ac9693c5a0da14e4ae3728e46d8cd33d
Christopher Donati <cdonati89@gmail.com>
2017-10-29 21:46:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var recipes = {
 function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({}, obj, { [key]: value });
   }
-function destructivelyUpdateObjectWithKeyAndValue(onj, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   obj[key] = value;
   return obj;
 }
